7c8d08a207f61fec7bf298529080f5969fdb67f1
Jukka Koivu <c2koju00@students.oamk.fi>
2017-03-21 09:53:54 +0200

    Base frame for the application added and meeting shedule view started

---
+++ Client/src/pages/contact/testpage.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-test',
+  templateUrl: 'testpage.html',
+  styles: [`
+    .spacer { padding: 0.2em; }
+    `]
+})
+
+export class TestPage {
+  
+  meetings: any;
+  times: any;
+
+  meetings2: any;
+  times2: any;
+
+  Date: any;
+
+  constructor(public navCtrl: NavController) {
+   
+    this.Date = '03-25-2017';
+
+    this.meetings = [
+    { 
+    meeting: 'Sprint Meeting', 
+    time: 9.15 
+    },
+    { 
+    meeting: 'Product Presentation', 
+    time: 12.15 
+    }
+    ];
+
+
+    this.meetings2 = [
+
+    { 
+    meeting: 'Business Meeting', 
+    time: 14.15 
+    },
+    { 
+    meeting: 'Product Presentation', 
+    time: 15.45 
+    },
+    { 
+    meeting: 'Team Meeting', 
+    time: 18.15 
+    },
+    { 
+    meeting: 'Job Conference', 
+    time: 19.45 
+    }
+    ];
+
+  }
+
+}
+
