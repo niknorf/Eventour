4f194565c56b85159f398ec35021a99d9480cd23
Jukka Koivu <c2koju00@students.oamk.fi>
2017-03-21 13:39:56 +0200

    new branch created

--- Client/src/pages/contact/testpage.ts
+++ Client/src/pages/contact/testpage.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
+import { ModalPage } from './modal-page';
 
 @Component({
   selector: 'page-test',
@@ -29,11 +30,13 @@ export class TestPage {
     this.meetings = [
     { 
     meeting: 'Sprint Meeting', 
-    starttime: 9.15 
+    starttime: 9.15,
+    endtime: 10.45
     },
     { 
     meeting: 'Product Presentation', 
-    starttime: 12.15 
+    starttime: 12.15,
+    endtime: 16.35
     }
     ];
 
@@ -41,25 +44,37 @@ export class TestPage {
     this.meetings2 = [
     { 
     meeting: 'Business Meeting', 
-    starttime: 14.15 
+    starttime: 14.15,
+    endtime: 15.35
     },
     { 
     meeting: 'Product Presentation', 
-    starttime: 15.45 
+    starttime: 15.45,
+    endtime: 16.45
     },
     { 
     meeting: 'Team Meeting', 
-    starttime: 18.15 
+    starttime: 18.15,
+    endtime: 18.45
     },
     { 
     meeting: 'Job Conference', 
-    starttime: 19.45 
+    starttime: 19.45,
+    endtime: 20.45
     }
     ];
 
   }
 
 }
-   
-    
+
+export class MyPage {
+  constructor(public modalCtrl: ModalController) {
+  }
+
+  presentModal() {
+    let modal = this.modalCtrl.create(ModalPage);
+    modal.present();
+  }
+}
 
